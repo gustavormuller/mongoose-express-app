8ff59509080a2b3620478ad8cff136db004c894a
Gustavo Muller <gusrmuller@gmail.com>
2023-06-21 02:02:32 -0300

    create products index

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+})
+
 app.listen(3000, () => {
     console.log('app is listening on port 3000!');
 })
